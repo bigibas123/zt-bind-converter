aa68f97b7e03d782cedd9eccc0ee1b0afcdf7ff9
Bas Dingemans <sebastiaandingemans@gmail.com>
2020-07-04 01:34:48 +0200

    added actual checks for main instead of just suggestions

--- src/main/java/com/github/bigibas123/zt/bind/converter/Main.java
+++ src/main/java/com/github/bigibas123/zt/bind/converter/Main.java
@@ -18,14 +18,17 @@ public class Main {
 		Logger log = LoggerFactory.getLogger("Environment Check");
 		boolean correct = true;
 		if (Reference.API_TOKEN == null) {
+			correct = false;
 			log.info("Environment variable \"ZT_TOKEN\" not set \n" +
 				"this is needed to access the api");
 		}
 		if (Reference.DOMAIN_PREFIX == null) {
+			correct = false;
 			log.info("Environment variable \"MAIN_DOMAIN\" not set \n" +
 				"this is needed to know where to end all records with");
 		}
 		if (Reference.PRIMARY_NS == null) {
+			correct = false;
 			log.info("Environment variable \"PRIMARY_NS\" not set \n" +
 				"this is needed for the NS records and the SOA");
 		}
